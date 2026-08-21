39a927c890e8257c91dc8669aeedfd2b8ea8833f
discombulatedkylian <discombulatedkylian@gmail.com>
2026-08-21 12:00:57 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: ":tools: Just moderating the server...", // required by Discord API, not shown in the client
         state: "Overseeing",     // this is what people actually see
         type: 3,               // Custom
       },
