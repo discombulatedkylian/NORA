206b6c4c97fa7794d8386aa7f46da451bab3eb61
discombulatedkylian <discombulatedkylian@gmail.com>
2026-08-21 13:55:07 +0100

    Update embedbuilder.js

--- src/commands/Tools/embedbuilder.js
+++ src/commands/Tools/embedbuilder.js
@@ -474,6 +474,7 @@ async function handleSetFooter(selectInteraction, rootInteraction, state) {
 }
 
 async function handleSetImages(selectInteraction, rootInteraction, state) {
+    await selectInteraction.deferUpdate();
     await selectInteraction.deferUpdate().catch(() => {});
 
     const imageSelect = new StringSelectMenuBuilder()
